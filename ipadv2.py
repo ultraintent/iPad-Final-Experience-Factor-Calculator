95a78b5dd7ff713d241db4906758d8382ccc75ce
ultraintent <52758403+ultraintent@users.noreply.github.com>
2020-07-31 00:30:06 -0700

    Made better

--- ipadv2.py
+++ ipadv2.py
@@ -1,36 +1,42 @@
 import math
+done=True
+def ipadv2():
+    # fetch price
+    price=input("What's the final price? ")
+    price=float(price)
+    price=round(price,2)
+    print("Price is $" + str(price) + ";\n")
 
-# fetch price
-price=input("What's the final price? ")
-price=float(price)
-price=round(price,2)
-print("Price is $" + str(price) + ";")
+    #fetch storage
+    storage=input("What's the storage? (GB) ")
+    storage=int(storage)
+    storage=round(storage,0)
+    print("Storage is " + str(storage) + "GB;\n")
 
-#fetch storage
-storage=input("What's the storage? (GB) ")
-storage=int(storage)
-storage=round(storage,0)
-print("Storage is " + str(storage) + "GB;")
+    #fetch generation
+    generation=input("What's the generation? ")
+    generation=int(generation)
+    generation=round(generation,0)
+    print("Generation is: " + str(generation) + ";\n")
 
-#fetch generation
-generation=input("What's the generation? ")
-generation=int(generation)
-generation=round(generation,0)
-print("Generation is: " + str(generation) + ";")
+    #convert to factors
+    pricefactor=price/10
+    storagefactor=(math.sqrt(storage))
+    generationfactor=(math.sqrt(64*generation))
 
-#convert to factors
-pricefactor=price/10
-storagefactor=(math.sqrt(storage))
-generationfactor=(math.sqrt(64*generation))
-
-#print factors
-print("pricefactor is " + str(pricefactor))
-print("storagefactor is " + str(storagefactor))
-print("generationfactor is " + str(generationfactor))
+    #print factors
+    #print("pricefactor is " + str(pricefactor))
+    #print("storagefactor is " + str(storagefactor))
+    #print("generationfactor is " + str(generationfactor))
 
 
-#calculate FEF
-fef=100*(storagefactor+generationfactor)/pricefactor
-print("FEF is " + str(fef) + "!")
+    #calculate FEF
+    fef=100*(storagefactor+generationfactor)/pricefactor
+    print("FEF is " + str(fef) + "!\n")
+
+    input("Press enter to repeat\n")
+    done=True
+while done:
+    ipadv2()
 
 
